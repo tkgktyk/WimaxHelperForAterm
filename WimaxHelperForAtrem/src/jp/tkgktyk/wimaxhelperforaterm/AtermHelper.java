1f1b67e4b7e392a2ce984d146f17b1cb7894a7e3
tkgktyk <tkgktyk@hotmail.co.jp>
2013-06-30 23:57:32 +0900

    add blocker checking on WakeUpService#_receiver#onReceive.

--- WimaxHelperForAtrem/src/jp/tkgktyk/wimaxhelperforaterm/AtermHelper.java
+++ WimaxHelperForAtrem/src/jp/tkgktyk/wimaxhelperforaterm/AtermHelper.java
@@ -395,6 +395,8 @@ public class AtermHelper {
 			public void onReceive(Context context, Intent intent) {
 				final String action = intent.getAction();
 				
+				if (_wakeUpLocked == false)
+					return;
 				if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
 					final int state
 					= intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
